ff26f4344f84e34f002a2fcc52585115c8e6bb6f
lifeflavor0425 <78671072+lifeflavor0425@users.noreply.github.com>
2022-10-28 17:29:05 +0900

    Create 모음 제거.js

---
+++ 프로그래머스/Lv0/모음 제거.js
@@ -0,0 +1,12 @@
+// 영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다. 문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(my_string) {
+    var answer = '';
+    let mouum = ['a','e','i','o','u'];
+    mouum.map(x=>{
+        my_string = my_string.split(x).join('')
+    })
+    return answer = my_string
+    
+    // return my_string.replace(/[aeiou]/g, '');
+}

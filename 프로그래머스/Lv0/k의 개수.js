1cc51606cd1f9abba8744ddf12064a4a607336f7
lifeflavor0425 <78671072+lifeflavor0425@users.noreply.github.com>
2022-11-12 15:00:48 +0900

    Create k의 개수.js

---
+++ 프로그래머스/Lv0/k의 개수.js
@@ -0,0 +1,20 @@
+// 1부터 13까지의 수에서, 1은 1, 10, 11, 12, 13 이렇게 총 6번 등장합니다. 정수 i, j, k가 매개변수로 주어질 때, i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
+
+
+function solution(i, j, k) {
+    var answer = 0;
+    while(i<=j){
+        answer += i.toString().split('').reduce((count,index)=>{
+            return Number(index) === k ? count +=1 : count
+        },0)
+        i+=1
+    }
+    return answer;
+    // 문자열 이용
+    // let a ='';
+    // for(i;i<=j;i++){
+    //     a += i;
+    // }
+    // console.log(a.split(k))
+    // return a.split(k).length-1;
+}
